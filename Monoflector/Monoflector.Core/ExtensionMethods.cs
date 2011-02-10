b5670401c6d55ef35233ad770b1acc2c19e1521f
Andrew Powell <andrew@shellscape.org>
2011-02-10 11:41:22 -0500

    added extension method to list derrived types in a list assemblies

--- Monoflector/Monoflector.Core/ExtensionMethods.cs
+++ Monoflector/Monoflector.Core/ExtensionMethods.cs
@@ -3,12 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Monoflector
-{
-    /// <summary>
-    /// Represents extension methods used by Monoflector.
-    /// </summary>
-    public static partial class ExtensionMethods
-    {
-    }
+using Mono.Cecil;
+
+namespace Monoflector {
+	
+	/// <summary>
+	/// Represents extension methods used by Monoflector.
+	/// </summary>
+	public static partial class ExtensionMethods {
+
+		private static Dictionary<String, List<TypeDefinition>> _derivedTypes = new Dictionary<String,List<TypeDefinition>>();
+		
+		public static List<TypeDefinition> FindDerivedTypes(this TypeDefinition typeDef, List<AssemblyDefinition> assemblies) {
+
+			String key = typeDef.MetadataToken.RID.ToString(String.Format("x0{0}", 6)).ToLower();
+
+			if (_derivedTypes.ContainsKey(key)) {
+				return _derivedTypes[key];
+			}
+
+			List<TypeDefinition> list = new List<TypeDefinition>();
+
+			foreach (var assembly in assemblies) {
+				foreach (var module in assembly.Modules) {
+					foreach (var type in module.Types) {
+						Boolean typeAdded = false;
+
+						if (type.BaseType != null) {
+							if (type.BaseType.FullName == typeDef.FullName) {
+								list.Add(type);
+								typeAdded = true;
+							}
+						}
+
+						if (!typeAdded && type.HasInterfaces) {
+							foreach (var iface in type.Interfaces) {
+								if (iface.FullName == typeDef.FullName) {
+									list.Add(type);
+									break;
+								}
+							}
+						}
+					}
+				}
+			}
+
+			_derivedTypes.Add(key, list);
+
+			return list;
+		}
+
+	}
 }
